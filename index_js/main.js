c50d8319d21faa108c4e59c6e82e1f7acb153202
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-01-04 18:15:16 +0800

    JavaScript调整
    将JS指令移出index，以.js文件存储

---
+++ index_js/main.js
@@ -0,0 +1,33 @@
+function compressed_grass_download() {
+    window.open("/html/CGR_Download/CGR_download.html", "_blank");
+}
+function compressed_grass_forge_github() {
+    window.open("https://github.com/Grey-Wind/CompressedGrassReborn-Forge", "_blank");
+}
+function compressed_grass_forge_gitee() {
+    window.open("https://gitee.com/at-studio/compressed-grass-reborn", "_blank");
+}
+function compressed_grass_fabric_github() {
+    window.open("https://github.com/Grey-Wind/CompressedGrassReborn-Fabric", "_blank");
+}
+function compressed_grass_fabric_gitee() {
+    window.open("https://gitee.com/at-studio/CompressedGrassReborn-Fabric", "_blank");
+}
+function more_synthetic_table() {
+    window.open("https://github.com/Grey-Wind/More_synthetic_table", "_blank");
+}
+function yuwen_fuxi() {
+    window.open("https://grey-wind.github.io/yuwen_fuxi/README.html", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function unfinished() {
+    alert("此项目未完成,请谅解");
+}
