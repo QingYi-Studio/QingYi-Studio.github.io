3df388e55ff099f32ed7e656edc533de73de409a
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-01-04 18:21:40 +0800

    调整CGR下载页面js代码位置

---
+++ html/CGR/js/cgr_download.js
@@ -0,0 +1,24 @@
+function forge_1_19() {
+    window.open("https://github.com/Grey-Wind/CompressedGrassReborn-Forge/releases/download/v6.3.0/CompressedGrassReborn-v6.3.0.jar")
+}
+function forge_1_18() {
+    window.open("https://github.com/Grey-Wind/CompressedGrassReborn-Forge/releases/download/v6.2.0/CompressedGrassReborn-v6.2.0.jar")
+}
+function forge_1_16() {
+    window.open("https://github.com/Grey-Wind/CompressedGrassReborn-Forge/releases/download/v6.0.0/CompressedGrassReborn-v6.0.0.jar")
+}
+function fabric_1_19(){
+    window.open("https://github.com/Grey-Wind/CompressedGrassReborn-Fabric/releases/download/v1.1.1/CompressedGrass-1.19.1-v1.1.1.jar")
+}
+function forge_1_19_mirror() {
+    window.open("https://gitee.com/at-studio/Mods/blob/main/CompressedGrass/CompressedGrassReborn-v6.6.0-1.19.2.jar")
+}
+function forge_1_18_mirror() {
+    window.open("https://gitee.com/at-studio/Mods/blob/main/CompressedGrass/CompressedGrassReborn-v6.2.0-1.18.2.jar")
+}
+function forge_1_16_mirror() {
+    window.open("https://gitee.com/at-studio/Mods/raw/main/CompressedGrass/CompressedGrassReborn-v6.0.0-1.16.5.jar")
+}
+function fabric_1_19(){
+    window.open("https://gitee.com/at-studio/Mods/raw/main/CompressedGrass(Fabric)/CompressedGrass-1.19.1-v1.2.0-pre1.jar")
+}
