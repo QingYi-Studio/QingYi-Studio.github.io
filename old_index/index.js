ceb695c5b0ad1794f25b574b666c4298c86aeff5
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-07-05 20:53:56 +0800

    新主页

---
+++ old_index/index.js
@@ -0,0 +1,155 @@
+// Compressed Grass Download
+function compressed_grass_download() {
+    window.open("./html/CGR_download/", "_blank");
+}
+// Compressed Grass Forge Download
+function compressed_grass_forge_github() {
+    window.open("https://github.com/Grey-Wind/CompressedGrassReborn-Forge", "_blank");
+}
+// Compressed Grass Forge Gitee
+function compressed_grass_forge_gitee() {
+    window.open("https://gitee.com/at-studio/compressed-grass-reborn", "_blank");
+}
+// Compressed Grass Fabric Github
+function compressed_grass_fabric_github() {
+    window.open("https://github.com/Grey-Wind/CompressedGrassReborn-Fabric", "_blank");
+}
+// Compressed Grass Fabric Gitee
+function compressed_grass_fabric_gitee() {
+    window.open("https://gitee.com/at-studio/CompressedGrassReborn-Fabric", "_blank");
+}
+// More Synthetic Table
+function more_synthetic_table() {
+    window.open("https://github.com/Grey-Wind/More_synthetic_table", "_blank");
+}
+// MinecraftModBuildHelper
+function minecraft_mod_build_helper() {
+    window.open("https://github.com/Grey-Wind/MinecraftModBuildHelper", "_blank");
+}
+// v2rayN
+function v2rayn() {
+    window.open("https://github.com/Grey-Wind/v2rayN-SystemAgentConfiguration", "_blank");
+}
+function novels() {
+    window.open("https://grey-wind.github.io/Novels/", "_blank");
+}
+function files() {
+    window.open("https://grey-wind.github.io/Files/", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function example() {
+    window.open("", "_blank");
+}
+function hot_word() {
+    window.open("", "_blank");
+}
+function yuwen_fuxi() {
+    window.open("https://grey-wind.github.io/FuXi/README.html", "_blank");
+}
